209f7f3209c9acc6adc6cf3c0eb4195f31898da5
alsonbasnet7 <basnetalson121@gmail.com>
2025-10-09 21:50:05 +0545

    Add marks.java and marks.class

---
+++ array/marks.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class marks{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int[] marks= new int[3];
+        marks[0]=25;//Nepali
+        marks[1]=30;//Maths
+        marks[2]=40;//Chem
+      System.out.println(marks[0]);        
+      System.out.println(marks[1]);        
+      System.out.println(marks[2]);        
+    }
+}
